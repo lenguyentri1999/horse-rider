0c7946615ac6e073af3c1dec7fefaf112f06b1b8
Tri Le <tnl34@drexel.edu>
2019-11-04 22:36:26 -0500

    prepare for merge to current

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,20 +4,24 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    redirectTo: '/launch', pathMatch: 'full',
+    redirectTo: '/landing', pathMatch: 'full',
   },
-  // { path: 'camp-info/:id', loadChildren: './pages/camp-info/camp-info.module#CampInfoPageModule', pathMatch: 'full' },
-  // { path: 'landing', loadChildren: './pages/landing/landing.module#LandingPageModule' },
+  {
+    path: 'camp-info/:id',
+    loadChildren: './pages/camp-info/camp-info.module#CampInfoPageModule',
+    pathMatch: 'full'
+  },
+  { path: 'landing', loadChildren: './pages/landing/landing.module#LandingPageModule' },
   // {
   //   path: '',
   //   loadChildren: () => import('./pages/tabs/tabs.module').then(m => m.TabsPageModule),
   // },
-  // {
-  //   path: 'login', loadChildren: './pages/login/login.module#LoginPageModule',
-  // },
-  // {
-  //   path: 'register', loadChildren: './pages/register/register.module#RegisterPageModule',
-  // },
+  {
+    path: 'login', loadChildren: './pages/login/login.module#LoginPageModule',
+  },
+  {
+    path: 'register', loadChildren: './pages/register/register.module#RegisterPageModule',
+  },
   { path: 'launch', loadChildren: './pages/launch/launch.module#LaunchPageModule' },
 
 ];
