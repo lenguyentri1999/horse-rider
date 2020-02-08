d43c5671f5c1be5245d96fe370d4c4f620ec2570
Tri Le <michaelle7312@gmail.com>
2020-02-08 13:22:34 -0500

    holy shit the clusters are done

--- src/app/components-mapbox/mapboxgl-map/mapboxgl-map.component.ts
+++ src/app/components-mapbox/mapboxgl-map/mapboxgl-map.component.ts
@@ -2,8 +2,8 @@ import * as mapboxgl from 'mapbox-gl';
 import { Component, OnInit, Input, OnChanges, SimpleChanges, SimpleChange, OnDestroy, AfterViewInit, ViewChild } from '@angular/core';
 import { environment } from 'src/environments/environment';
 import { of, Observable, Subject, combineLatest, forkJoin } from 'rxjs';
-import { MapboxMap, MapboxMapOptions } from 'src/models/mapboxMap';
-import { MapboxPlace } from 'src/models/mapboxResult';
+import { MapboxMap, MapboxMapOptions, MapboxSourceOptions } from 'src/models/mapboxMap';
+import { MapboxPlace, MapboxSource } from 'src/models/mapboxResult';
 import { tap, flatMap, switchMap } from 'rxjs/operators';
 import { DbService } from 'src/app/services/db.service';
 
@@ -96,17 +96,137 @@ export class MapboxglMapComponent implements OnInit, OnChanges, AfterViewInit {
   private async populateMarkers(myMap: MapboxMap, geoJsonData: MapboxPlace[]) {
     await this.removeAllMarkers();
 
-    // make a marker for each feature and add to the map
-    geoJsonData.forEach(place => {
-      // console.log(place);
-      const coords = place.geometry.coordinates;
-      const marker = new mapboxgl.Marker()
-        .setLngLat(coords);
-      marker.setPopup(new mapboxgl.Popup({ offset: 25 }) // add popups
-        .setHTML(`<a href='camp-info/${place.properties.id}'>` + place.properties.title + '</a>'));
-      marker.addTo(myMap);
-      this.markers.push(marker);
+    const sourceName = 'CampData';
+
+
+    // myMap.addSource('earthquakes', {
+    //   type: 'geojson',
+    //   // Point to GeoJSON data. This example visualizes all M1.0+ earthquakes
+    //   // from 12/22/15 to 1/21/16 as logged by USGS' Earthquake hazards program.
+    //   data: source,
+    //   cluster: true,
+    //   clusterMaxZoom: 14, // Max zoom to cluster points on
+    //   clusterRadius: 50 // Radius of each cluster when clustering points (defaults to 50)
+    // })
+
+    const options: MapboxSourceOptions = {
+      type: 'geojson',
+      data: {
+        type: 'FeatureCollection',
+        features: geoJsonData
+      },
+      cluster: true,
+      clusterMaxZoom: 14,
+      clusterRadius: 50,
+    };
+
+    myMap.addSource(sourceName, options);
+    myMap.addLayer({
+      id: 'clusters',
+      type: 'circle',
+      source: sourceName,
+      filter: ['has', 'point_count'],
+      paint: {
+        // Use step expressions (https://docs.mapbox.com/mapbox-gl-js/style-spec/#expressions-step)
+        // with three steps to implement three types of circles:
+        //   * Blue, 20px circles when point count is less than 100
+        //   * Yellow, 30px circles when point count is between 100 and 750
+        //   * Pink, 40px circles when point count is greater than or equal to 750
+        'circle-color': [
+          'step',
+          ['get', 'point_count'],
+          '#51bbd6',
+          100,
+          '#f1f075',
+          750,
+          '#f28cb1'
+        ],
+        'circle-radius': [
+          'step',
+          ['get', 'point_count'],
+          20,
+          100,
+          30,
+          750,
+          40
+        ]
+      }
     });
+
+    myMap.addLayer({
+      id: 'cluster-count',
+      type: 'symbol',
+      source: sourceName,
+      filter: ['has', 'point_count'],
+      layout: {
+        'text-field': '{point_count_abbreviated}',
+        'text-font': ['DIN Offc Pro Medium', 'Arial Unicode MS Bold'],
+        'text-size': 12
+      }
+    });
+
+    myMap.addLayer({
+      id: 'unclustered-point',
+      type: 'circle',
+      source: sourceName,
+      filter: ['!', ['has', 'point_count']],
+      paint: {
+        'circle-color': '#11b4da',
+        'circle-radius': 8,
+        'circle-stroke-width': 2,
+        'circle-stroke-color': '#fff'
+      }
+    });
+
+    // inspect a cluster on click
+    myMap.on('click', 'clusters', function (e) {
+      var features = myMap.queryRenderedFeatures(e.point, {
+        layers: ['clusters']
+      });
+      var clusterId = features[0].properties.cluster_id;
+      myMap.getSource(sourceName).getClusterExpansionZoom(
+        clusterId,
+        function (err, zoom) {
+          if (err) return;
+
+          myMap.easeTo({
+            center: features[0].geometry.coordinates,
+            zoom: zoom
+          });
+        }
+      );
+    });
+
+    myMap.on('click', 'unclustered-point', function (e) {
+      const feature: MapboxPlace = e.features[0];
+      feature.properties.id
+      // console.log(e);
+      new mapboxgl.Popup({ offset: 25 }) 
+      .setLngLat(feature.geometry.coordinates)
+      .setHTML(`<a href='camp-info/${feature.properties.id}'>` + feature.properties.title + '</a>')
+      .addTo(myMap);
+
+    })
+
+    myMap.on('mouseenter', 'clusters', function () {
+      myMap.getCanvas().style.cursor = 'pointer';
+    });
+    myMap.on('mouseleave', 'clusters', function () {
+      myMap.getCanvas().style.cursor = '';
+    });
+
+
+    // make a marker for each feature and add to the map
+    // geoJsonData.forEach(place => {
+    //   // console.log(place);
+    //   const coords = place.geometry.coordinates;
+    //   const marker = new mapboxgl.Marker()
+    //     .setLngLat(coords);
+    //   marker.setPopup(new mapboxgl.Popup({ offset: 25 }) // add popups
+    //     .setHTML(`<a href='camp-info/${place.properties.id}'>` + place.properties.title + '</a>'));
+    //   marker.addTo(myMap);
+    //   this.markers.push(marker);
+    // });
   }
 
   private isMapLoaded(myMap: MapboxMap): Promise<MapboxMap> {
